23359990e94f45d882bf05d78cf684536fdffbcf
Christopher <chromarphun@gmail.com>
2021-03-12 20:32:55 -0600

    file system started

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-fn main() {
-    let x=5;
-    let mut y=x;
-    println!("{}", y);
-    y=y+1;
-    println!("{}",x);
-    println!("{}", y);
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
+
+fn main() -> io::Result<()> {
+    let mut memory: [u8; 4096]=[0; 4096];
+    let mut f = File::open("tests/exp")?;
+    f.read(&mut memory)?;
+    println!("{}", memory[4]);
+    Ok(())
 }
